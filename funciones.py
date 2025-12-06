a7b5f028124cddd4f106a4fbb8b332520449e8d6
xavicrip <wxbustamante@utpl.edu.ec>
2025-12-05 23:14:40 -0500

    ejemplos funciones

---
+++ funciones.py
@@ -0,0 +1,24 @@
+# ============================================
+# FUNCIONES
+# ============================================
+
+# Funcion que devuelve valor
+
+def sumar(a,b):
+    resultado = a+b
+    return resultado
+
+print(sumar(2,3))
+print(sumar(300,900))
+print(sumar(-2,9))
+print(sumar(30,-900))
+
+# Funiones que no devuelven valor
+
+def saludar(nombre):
+    print("Hola", nombre)
+
+saludar("Wayner")
+saludar("Byron")
+saludar("Estefany")
+saludar("Xavier")
